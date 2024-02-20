27902f32db70458ce1738450d0eb80040855af88
TO <82508487+TT665cc@users.noreply.github.com>
2024-02-20 20:59:37 +0100

    Delete Supprime_tout directory

--- Supprime_tout/background.js
+++
@@ -1,8 +0,0 @@
-// background.js
-var permissionState = false;
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === 'permissionResponse') {
-    permissionState = request.granted;
-  }
-});
